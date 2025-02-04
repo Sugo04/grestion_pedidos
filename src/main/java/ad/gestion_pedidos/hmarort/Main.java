1941e041d9d1d123db331c77094ef1080540a3a3
Héctor <hmarort075@g.educaand.es>
2025-02-04 10:36:08 +0100

    Editado archivo Main

--- src/main/java/ad/gestion_pedidos/hmarort/Main.java
+++ src/main/java/ad/gestion_pedidos/hmarort/Main.java
@@ -4,6 +4,6 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("Inicio de la aplicación de gestion de la bd 'pedidos.sql'...");
     }
 }

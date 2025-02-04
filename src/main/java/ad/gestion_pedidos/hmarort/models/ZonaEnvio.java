fd923b1cdd00085b10af313506231dc9d2b7e094
Héctor <hmarort075@g.educaand.es>
2025-02-04 10:58:10 +0100

    Editado archivo ZonaEnvio de la carpeta models

--- src/main/java/ad/gestion_pedidos/hmarort/models/ZonaEnvio.java
+++ src/main/java/ad/gestion_pedidos/hmarort/models/ZonaEnvio.java
@@ -1,5 +1,41 @@
 package ad.gestion_pedidos.hmarort.models;
 
 public class ZonaEnvio {
+    private int id;
+    private String nombre;
+    private double precio;
+
+    public ZonaEnvio() {
+    }
+
+    public ZonaEnvio(int id, String nombre, double precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 
 }

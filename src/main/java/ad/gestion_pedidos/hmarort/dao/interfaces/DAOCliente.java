0dfd31b18e4aa1a60ef0ee1310bb6636e11934a3
Héctor <hmarort075@g.educaand.es>
2025-02-04 13:30:58 +0100

    Editado DAOCliente

--- src/main/java/ad/gestion_pedidos/hmarort/dao/interfaces/DAOCliente.java
+++ src/main/java/ad/gestion_pedidos/hmarort/dao/interfaces/DAOCliente.java
@@ -1,5 +1,22 @@
 package ad.gestion_pedidos.hmarort.dao.interfaces;
 
+import java.util.List;
+
+import ad.gestion_pedidos.hmarort.models.Cliente;
+
 public interface DAOCliente {
 
+    void insertarCliente(Cliente cliente) throws Exception;
+
+    Cliente buscarCliente(int id) throws Exception;
+
+    List<Cliente> listarClientes() throws Exception;
+
+    void actualizarCliente(Cliente cliente) throws Exception;
+
+    void eliminarCliente(int id) throws Exception;
+
+    Cliente buscarClientePorZona(int idZona) throws Exception;
+
+    double calcularFacturacionCliente(int idCliente) throws Exception;
 }

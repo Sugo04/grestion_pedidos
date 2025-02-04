423009c76bcfa15f1e1ba1a0bf850a9ef040b699
Héctor <hmarort075@g.educaand.es>
2025-02-04 10:44:36 +0100

    Editados archivos UI, UIAutoImpl y UIManualImpl  al agregar las funciones en la interfaz UI.

--- src/main/java/ad/gestion_pedidos/hmarort/ui/UI.java
+++ src/main/java/ad/gestion_pedidos/hmarort/ui/UI.java
@@ -1,5 +1,19 @@
 package ad.gestion_pedidos.hmarort.ui;
 
 public interface UI {
-    
+    void iniciar();
+
+    void mostrarMensaje(String mensaje);
+
+    void mostrarError(String mensaje);
+
+    int mostrarMenu();
+
+    void gestionarClientes();
+
+    void gestionarPedidos();
+
+    void consultarZonasEnvio() throws Exception;
+
+    void consultarPedidosCliente() throws Exception;
 }
--- src/main/java/ad/gestion_pedidos/hmarort/ui/UIAutoImpl.java
+++ src/main/java/ad/gestion_pedidos/hmarort/ui/UIAutoImpl.java
@@ -2,4 +2,52 @@ package ad.gestion_pedidos.hmarort.ui;
 
 public class UIAutoImpl implements UI{
 
+    @Override
+    public void consultarPedidosCliente() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void consultarZonasEnvio() throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void gestionarClientes() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void gestionarPedidos() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void iniciar() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mostrarError(String mensaje) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mostrarMensaje(String mensaje) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int mostrarMenu() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
 }

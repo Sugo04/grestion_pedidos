02efaedb45d6c93224493371546ff7f5b950fde5
Héctor <hmarort075@g.educaand.es>
2025-02-04 10:54:51 +0100

    Editado archivo Pedido en carpeta models

--- src/main/java/ad/gestion_pedidos/hmarort/models/Pedido.java
+++ src/main/java/ad/gestion_pedidos/hmarort/models/Pedido.java
@@ -1,5 +1,53 @@
 package ad.gestion_pedidos.hmarort.models;
 
+import java.time.LocalDate;
+
 public class Pedido {
+    private int id;
+    private LocalDate fecha;
+    private double importe;
+    private int idCliente;
+
+    public Pedido() {
+    }
+
+    public Pedido(int id, LocalDate fecha, double importe, int idCliente) {
+        this.id = id;
+        this.fecha = fecha;
+        this.importe = importe;
+        this.idCliente = idCliente;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
 
 }

6b0c20c22bf4989c215b9247cf4fd9a5821552a3
Héctor <hmarort075@g.educaand.es>
2025-02-04 13:34:25 +0100

    Editado DAOPedido

--- src/main/java/ad/gestion_pedidos/hmarort/dao/interfaces/DAOPedido.java
+++ src/main/java/ad/gestion_pedidos/hmarort/dao/interfaces/DAOPedido.java
@@ -1,5 +1,25 @@
 package ad.gestion_pedidos.hmarort.dao.interfaces;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import ad.gestion_pedidos.hmarort.models.Pedido;
+
 public interface DAOPedido {
 
+    void insertarPedido(Pedido pedido) throws Exception;
+
+    void eliminarPedido(Pedido pedido) throws Exception;
+
+    void modificarPedido(Pedido pedido) throws Exception;
+
+    Pedido buscarPedido(int id) throws Exception;
+
+    List<Pedido> listarPedidos() throws Exception;
+
+    List<Pedido> listarPedidosPorCliente(int idCliente) throws Exception;
+
+    List<Pedido> listarPedidosPorFecha(LocalDate fecha) throws Exception;
+
+    double calcularTotalPedidosPorCliente(int idCliente) throws Exception;
 }

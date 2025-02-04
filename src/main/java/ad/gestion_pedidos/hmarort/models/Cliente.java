af4e821a7dfc397fa8364a2dbcea1839cd1e3b6a
Héctor <hmarort075@g.educaand.es>
2025-02-04 10:52:18 +0100

    Editado archivo Cliente de la carpeta models

--- src/main/java/ad/gestion_pedidos/hmarort/models/Cliente.java
+++ src/main/java/ad/gestion_pedidos/hmarort/models/Cliente.java
@@ -1,5 +1,61 @@
 package ad.gestion_pedidos.hmarort.models;
 
 public class Cliente {
+    private int id;
+    private String nombre;
+    private String email;
+    private String telefono;
+    private int idZonaEnvio;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nombre, String email, String telefono, int idZonaEnvio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.telefono = telefono;
+        this.idZonaEnvio = idZonaEnvio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getIdZonaEnvio() {
+        return idZonaEnvio;
+    }
+
+    public void setIdZonaEnvio(int idZonaEnvio) {
+        this.idZonaEnvio = idZonaEnvio;
+    }    
 
 }
